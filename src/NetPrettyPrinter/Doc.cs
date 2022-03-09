bfea1b9a3309b23376d290c99943f54cc0e76517
Florian Lorenzen <florian.lorenzen@posteo.de>
2022-03-09 22:23:09 +0100

    Place holder doc comments

--- src/NetPrettyPrinter/Doc.cs
+++ src/NetPrettyPrinter/Doc.cs
@@ -34,6 +34,9 @@ using Tok = NetPrettyPrinter.Token;
 
 namespace NetPrettyPrinter
 {
+    /// <summary>
+    ///    
+    /// </summary>
     public class Doc
     {
         private readonly Elem _elem;
@@ -43,6 +46,11 @@ namespace NetPrettyPrinter
             _elem = elem;
         }
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="lineWidth"></param>
+        /// <returns></returns>
         public string PrettyPrint(uint lineWidth = 80)
         {
             var pp = new PrettyPrint(Convert.ToInt32(lineWidth));
@@ -71,34 +79,225 @@ namespace NetPrettyPrinter
             return pp.GetValue();
         }
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
         public static Doc Text(string content) => NewDoc(Singleton(Tok.Text(content)));
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public static Doc Number(int num) => num.ToString();
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public static Doc Number(uint num) => num.ToString();
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public static Doc Number(short num) => num.ToString();
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
         public static Doc Number(ushort num) => num.ToString();
+        
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>        
         public static Doc Number(sbyte num) => num.ToString();
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>        
+
         public static Doc Number(byte num) => num.ToString();
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>        
+
         public static Doc Number(long num) => num.ToString();
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>        
+
         public static Doc Number(ulong num) => num.ToString();
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>        
+
         public static Doc Number(float num) => num.ToString(CultureInfo.InvariantCulture);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>        
+
         public static Doc Number(double num) => num.ToString(CultureInfo.InvariantCulture);
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
+
         public static Doc Number(decimal num) => num.ToString(CultureInfo.InvariantCulture);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
+
         public static Doc Timespan(TimeSpan span) => span.ToString();
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
+
         public static Doc Timestamp(DateTime timestamp) => timestamp.ToString("s", CultureInfo.InvariantCulture);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(string content) => Text(content);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(int num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(uint num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(short num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(ushort num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(sbyte num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(byte num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(long num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(ulong num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(float num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(double num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(decimal num) => Number(num);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(TimeSpan span) => Timespan(span);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>        
         public static implicit operator Doc(DateTime timestamp) => Timestamp(timestamp);
 
         /// <summary>
@@ -212,63 +411,259 @@ namespace NetPrettyPrinter
         /// <returns>composed document</returns>
         public static Doc operator ^(Doc top, Doc bottom) => top.Above(bottom);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc JoinCat(IEnumerable<Doc> docs, Doc sep) => JoinDocs(docs, (agg, doc) => agg + sep + doc);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc JoinSep(IEnumerable<Doc> docs, Doc sep) => JoinDocs(docs, (agg, doc) => agg + sep % doc);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc JoinCatOrAbove(IEnumerable<Doc> docs, Doc sep) => JoinDocs(docs, (agg, doc) => agg + sep | doc);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc JoinSepOrAbove(IEnumerable<Doc> docs, Doc sep) => JoinDocs(docs, (agg, doc) => agg + sep & doc);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc JoinAbove(IEnumerable<Doc> docs, Doc sep) => JoinDocs(docs, (agg, doc) => agg + sep ^ doc);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public Doc Break(uint amount) =>
             NewDoc(Singleton(Tok.Begin(Convert.ToInt32(amount))).Concat(_elem).Concat(Singleton(Tok.End())));
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc Break(uint amount, Doc doc) => doc.Break(amount);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public Doc Break() => Break(Tok.DefaultIndent);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc Break(Doc doc) => doc.Break();
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public Doc BreakAll(uint amount) => NewDoc(Singleton(Tok.Begin(Convert.ToInt32(amount), BreakType.Consistent))
             .Concat(_elem)
             .Concat(Singleton(Tok.End())));
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc BreakAll(uint amount, Doc doc) => doc.BreakAll(amount);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc BreakAll(Doc doc) => BreakAll(Tok.DefaultIndent, doc);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public Doc Indent(uint amount) => NewDoc(Singleton(Tok.Break(offset: Convert.ToInt32(amount)))
             .Concat(Singleton(Tok.Begin(0))).Concat(_elem)
             .Concat(Singleton(Tok.End())));
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc Indent(uint amount, Doc doc) => doc.Indent(amount);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public Doc Indent() => Indent(Tok.DefaultIndent);
+
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc Indent(Doc doc) => doc.Indent();
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public Doc IndentAll(uint amount) => NewDoc(Singleton(Tok.Break(offset: Convert.ToInt32(amount)))
             .Concat(Singleton(Tok.Begin(0, BreakType.Consistent))).Concat(_elem)
             .Concat(Singleton(Tok.End())));
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc IndentAll(uint amount, Doc doc) => doc.IndentAll(amount);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public Doc IndentAll() => IndentAll(Tok.DefaultIndent);
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc IndentAll(Doc doc) => doc.IndentAll();
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc InParens(Doc doc) => "(" + doc + ")";
 
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc Comma = ",";
+        
+        /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc Dot = ".";
+
+                /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc ParenOpen => "(";
+
+                /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc ParenClose => ")";
+
+                /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc Space => Spaces(1);
+
+                /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc Spaces(uint amount) => new string(' ', Convert.ToInt32(amount));
+
+                /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc NewLine => NewDoc(Singleton(Tok.LineBreak()));
+
+                /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         private static readonly Elem EmptyElem = new ElemEmpty();
+
+                /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static readonly Doc Empty = NewDoc(EmptyElem);
+
+                /// <summary>
+        ///    
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
         public static Doc ToDoc(bool visible, string text) => visible ? text : Empty;
 
         private static Doc NewDoc(Elem elem) => new Doc(elem);
